7cd877f53b4683c7ff0b0ba5cfab25da5a04748a
Kim Ashley Sunio <kimashleysunio@gmail.com>
2022-01-13 17:36:04 -1000

    added tags

--- PracticeOne.java
+++ PracticeOne.java
@@ -2,24 +2,24 @@ public class PracticeOne {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        // print "MOHS"
+        // A1: print "MOHS"
 
-        // multiply 4 times 4 and print the result
+        // A2: multiply 4 times 4 and print the result
 
-        // create an array named "arr" that contains 3, 5, 1, 6, 3, 3
+        // A3: create an array named "arr" that contains 3, 5, 1, 6, 3, 3
 
-        // create a 2D array named "array2D": { {1, 2, 3}, {4, 5, 6} }
+        // A4: create a 2D array named "array2D": { {1, 2, 3}, {4, 5, 6} }
 
     }
 
-    // write a method on the occurences of the number 3 in the array (and call it in
-    // the main method)
+    // M1: write a method on the occurences of the number 3 in the array
+    // (and call it in the main method)
 
-    // write a method that removes the last element of an array (and call it in the
-    // main method)
+    // M2: write a method that removes the last element of an array
+    // (and call it in the main method)
 
-    // write a method that adds the sum of all numbers in a array (and call it in
-    // the main method)
+    // M3: write a method that adds the sum of all numbers in a array
+    // (and call it in the main method)
 
-    // write a sorting algorithm (and call it in the main method)
+    // M4: write a sorting algorithm (and call it in the main method)
 }
