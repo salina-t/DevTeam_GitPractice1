6727e632059df567f2a773692d12bf52ee8a6d80
Kim Ashley Sunio <kimashleysunio@gmail.com>
2022-01-13 17:30:38 -1000

    added practice problems

---
+++ PracticeOne.java
@@ -0,0 +1,25 @@
+public class PracticeOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // print "MOHS"
+
+        // multiply 4 times 4 and print the result
+
+        // create an array named "arr" that contains 3, 5, 1, 6, 3, 3
+
+        // create a 2D array named "array2D": { {1, 2, 3}, {4, 5, 6} }
+
+    }
+
+    // write a method on the occurences of the number 3 in the array (and call it in
+    // the main method)
+
+    // write a method that removes the last element of an array (and call it in the
+    // main method)
+
+    // write a method that adds the sum of all numbers in a array (and call it in
+    // the main method)
+
+    // write a sorting algorithm (and call it in the main method)
+}
